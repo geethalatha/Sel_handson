fc0ba737960a578d3eb592cfa0c427ad52f4b373
Manga <gemanga@deloitte.com>
2019-05-15 18:16:30 +0530

    New change

--- src/module1/FirstJavaClassFile.java
+++ src/module1/FirstJavaClassFile.java
@@ -1,9 +1,22 @@
 package module1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstJavaClassFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		//Single Line
+		
+				/*
+				 * Multi line
+				 */
+				
+				//Initializing the browser
+	
 
 	}
 
---
+++ src/module2/SeleniumCommands.java
@@ -0,0 +1,17 @@
+package module2;
+
+public class SeleniumCommands {
+
+	public static void main(String[] args) {
+		
+		//Single Line
+		
+		/*
+		 * Multi line
+		 */
+		
+		//Initializing the browser
+		
+	}
+
+}
